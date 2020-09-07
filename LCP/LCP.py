13f7f5f5ecbd0cbbe47b3f85b402e7c5d13018f2
Phuc Nguyen <phucnguyen@Phuc-Nguyen-2.local>
2020-09-07 20:01:30 +0700

    LCP

---
+++ LCP/LCP.py
@@ -0,0 +1,16 @@
+def longestCommonPrefix(strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                print(other)
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest
+
+print(longestCommonPrefix(("phuc", "phe", "phon")))
